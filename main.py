9a82d8a9c61e5c6bb6769c02e31195319eb653b0
TomMahawk711 <thomas.klotz98@gmail.com>
2024-01-06 17:05:11 +0100

    add counter, refactor, trying with multi-disc albums

--- main.py
+++ main.py
@@ -2,29 +2,46 @@ import logging
 import os
 import re
 
-REGEX = r'^.+\/\d{4} - .+\/\d{2} - .+\..+$'
+ARTIST = r'.+'
+ALBUM = r'\d{4} - .+'
+DISC = r'CD(\d+)'
+TITLE = r'\d{2} - .+\..+'
+
+SINGLE_DISC_ALBUM = fr'^{ARTIST}\/{ALBUM}\/{TITLE}$'
+MULTI_DISC_ALBUM = fr'^{ARTIST}\/{ALBUM}\/{DISC}\/{TITLE}$'
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    _check_music_library()
+    _check_directories()
 
 
-def _check_music_library():
-    for root, dirs, files in os.walk('.'):
-        for file in files:
-            file_path = os.path.join(root, file)
+def _check_directories():
+    incorrect_directory_naming_counter = 0
 
-            if '.DS_Store' in file or _check_file_extension(file):
-                continue
+    for root, _, files in os.walk('.'):
+        incorrect_directory_naming_counter = check_directory(root, files, incorrect_directory_naming_counter)
 
-            if not re.match(REGEX, file_path):
-                logging.warning(f'FAIL: directory is not correct for -> {file_path}')
+    logging.info(f'incorrect instances: {incorrect_directory_naming_counter}')
 
 
-def _check_file_extension(filename):
+def check_directory(root, files, incorrect_directory_naming_counter):
+    for file in files:
+        file_path = os.path.join(root, file)
+
+        if '.DS_Store' in file or not _is_audio_file(file):
+            continue
+
+        if not re.match(SINGLE_DISC_ALBUM, file_path):
+            incorrect_directory_naming_counter += 1
+            logging.warning(f'directory incorrect: {file_path}')
+
+    return incorrect_directory_naming_counter
+
+
+def _is_audio_file(filename):
     _, file_extension = os.path.splitext(filename)
-    return '.mp3' not in file_extension and '.flac' not in file_extension
+    return '.mp3' in file_extension or '.flac' in file_extension
 
 
 if __name__ == '__main__':
