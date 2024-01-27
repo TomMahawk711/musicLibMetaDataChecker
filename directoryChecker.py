432dea77b06c5f72164cdf4394e5f7992dbe9a7c
UnHold (Alex) <alex@hold.re>
2024-01-27 11:35:47 +0100

    added check to ignore cover art

--- directoryChecker.py
+++ directoryChecker.py
@@ -20,6 +20,11 @@ ARTIST = r'.+'
 ALBUM = r'\d{4} - .+'
 DISC = r'CD\d+'
 TRACK = r'\d{2} - .+\.(mp3|flac)'
+COVER_IMG = r'.+\.(jpg|png)'
+
+IGNORE_COVER_IMG = True
+
+FILES_TO_IGNORE = ['.DS_Store']
 
 
 def check(absolute_path):
@@ -38,13 +43,17 @@ def _check_directory(root, files):
         file_path = os.path.join(root, file)
 
         # ignore those files
-        if file == '.DS_Store':
+        if file in FILES_TO_IGNORE:
             continue
 
         # quick check
         SEPERATOR = r'(\/|\\)'
         OPTIONAL_DISK = f'({DISC}{SEPERATOR})?'
         if re.match(f'\.{ARTIST}{SEPERATOR}{ALBUM}{SEPERATOR}{OPTIONAL_DISK}{TRACK}', file_path):
+            continue
+
+        # ignore cover images (need to lie with the album)
+        if IGNORE_COVER_IMG and re.match(f'\.{ARTIST}{SEPERATOR}{ALBUM}{SEPERATOR}{OPTIONAL_DISK}{COVER_IMG}', file_path):
             continue
 
         # at this point the quick check failed and we hava a invalid file
