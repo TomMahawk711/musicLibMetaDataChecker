0359a4775f28380c6c6105df4241dc2aef64c1dd
UnHold (Alex) <alex@hold.re>
2024-01-27 11:44:53 +0100

    added argparse for argument handling + updated readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # musicLibMetaDataChecker
+
 ### Usage
-put `main.py` into your music folder and run it
+
+```
+python3 main.py [-h] [-a] [--structure] music_folder
+
+positional arguments:
+  music_folder  Absolute path to the music folder
+
+options:
+  -h, --help    show this help message and exit
+  -a, --all     Run all the checks
+  --structure   Check if the folder structure complies
+```
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import logging
+import argparse
 import directoryChecker
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    directoryChecker.check(r"\\10.69.0.4\music_share")
+    parser = argparse.ArgumentParser(
+                    prog='python3 main.py',
+                    description='This program verifies multiple rules that we want to have enforced on the music library')
+    parser.add_argument('music_folder', help='Absolute path to the music folder')
+    parser.add_argument('-a', '--all', help="Run all the checks", action='store_true')
+    parser.add_argument('--structure', help="Check if the folder structure complies", action='store_true')
+
+    args = parser.parse_args()
+
+    # handle the arguments
+    if args.all or args.structure:
+        logging.info("Checking the folder structure")
+        directoryChecker.check(args.music_folder)
 
 if __name__ == '__main__':
     main()
